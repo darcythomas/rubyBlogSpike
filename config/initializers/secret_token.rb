a845576af9e60a8c7842b87557d1044e84dfbe10
Darcy <darcy@darcy-Ruby-Dev.(none)>
2012-08-11 13:57:07 +1200

    ruby blog ruby nights gig in Jafaland init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '569ac767529b8eedb5cfbd2a453dbb56642ba5ca9984440f7698889a2d41e4553a160a6b33c1c1f4f342335de084efa2e1501e1ba543a07b27cfc192e97bece9'

8517077be2120bead4289d5c00aea798d5096e84
Darcy <darcy@darcy-Ruby-Dev.(none)>
2012-08-13 16:02:50 +1200

    changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ActiveRecord::Base
    attr_accessible :title, :content
+
+   validates :title, :content, :slug, presence: true
+
+
+   before_save :create_slug
+
+
+   def create_slug
+   	self.slug = self.title.downcase.gsub(/[^a-z0-9]+/,'-')   	
+   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Blog::Application.routes.draw do
+  
+
+resources  :articles 
+
+match "/:slug" => "articles#show", as: 'slugged'
+
+root to:  "articles#index"
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
